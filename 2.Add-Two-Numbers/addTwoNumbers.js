982b827419fcc5f02dfad2e94ef69e98fdfe7ed0
Henry_Z <chancidy@gmail.com>
2016-08-21 15:49:01 +0000

    update

---
+++ 2.Add-Two-Numbers/addTwoNumbers.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+  function ListNode(val) {
+     this.val = val;
+     this.next = null;
+ }
+ 
+var addTwoNumbers = function(l1, l2) {
+    
+};
---
+++ 2.Add-Two-Numbers/example.js
@@ -0,0 +1,25 @@
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function ListNode(val) {
+     this.val = val;
+     this.next = null;
+};
+var addTwoNumbers = function(l1, l2) {
+    var dummyList = new ListNode(0);
+    var curr = dummyList;
+    var carry = 0, x, y, sum;
+    while(l1 !== null || l2 !== null || carry){
+    	x = (l1 !== null) ? l1.val : 0;
+    	y = (l2 !== null) ? l2.val : 0;
+    	sum = carry + x + y;
+    	carry = Math.floor(sum / 10);  
+    	curr.next = new ListNode(sum % 10);
+    	curr = curr.next;
+    	if (l1 !== null) l1 = l1.next;
+        if (l2 !== null) l2 = l2.next;
+    } 
+    return dummyList.next;
+};
