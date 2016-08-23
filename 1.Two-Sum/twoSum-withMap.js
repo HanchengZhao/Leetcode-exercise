0f25ba0f291a41bac96433557cedd2d49d2a2477
Henry_Z <chancidy@gmail.com>
2016-08-23 03:23:49 +0000

    update

--- 1.Two-Sum/twoSum-withMap.js
+++ 1.Two-Sum/twoSum-withMap.js
@@ -10,12 +10,11 @@ var twoSum = function(nums, target) {
     for(var i = 0; i < nums.length; ++i){
         
         var compliment = target - nums[i];
-        
         if(myMap.get(compliment) !== undefined){
-            console.log([myMap[compliment], i]);
-            return [myMap[compliment], i];
+            console.log([myMap.get(compliment), i]);
+            return [myMap.get(compliment), i];
         }
-        myMap.set(compliment, i);// compliment as key, i as position
+        myMap.set(nums[i], i);// num[i] as key, i as position
     }
     
     
@@ -26,4 +25,4 @@ var nums = [2, 7, 11, 15];
 var target = 9;
 twoSum(nums, target);
 
-//costs O(n^2)
+//costs O(n)
