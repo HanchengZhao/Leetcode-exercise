dcfab2507c99beb4912861b6c76788fc9f3ff617
Henry_Z <chancidy@gmail.com>
2016-08-31 13:20:43 +0000

    longest palindromic substring

---
+++ 5.longest_palindromic_substring/longestPalindrome.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function reverseString(s){
+    return s.split('').reverse().join(''); //only array in js can be reversed
+}
+var longestPalindrome = function(s) {
+    var length = s.length;
+    var str = '';
+    var longestPalin = '';
+    for(var i = 0; i < length; i++){
+        for(var j = i + 1; j <= length; j++){ //j <= length
+            str = s.substring(i,j);
+            var rev = reverseString(str);
+            if(str === rev){
+                if(str.length > longestPalin.length){
+                    longestPalin = str;
+                }
+            }
+        }
+    }
+    return longestPalin;
+};//takes O(n^3);
+
+// var a = "abcbafasdf";
+// longestPalindrome(a);
