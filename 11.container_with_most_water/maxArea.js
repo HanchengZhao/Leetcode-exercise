1e546f0fc56d34c139ab538bafcc605d3ef184bb
Henry_Z <chancidy@gmail.com>
2016-09-05 15:25:14 +0000

    two pointer maxArea solution

---
+++ 11.container_with_most_water/maxArea.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    var maxArea = 0;
+    var length = height.length;
+    var head = 0, tail = length-1;
+    while (tail - head > 0){
+        var area = Math.min(height[head],height[tail]) * (tail - head);
+        maxArea = Math.max(maxArea, area);
+        if (height[head] > height[tail]){
+            tail--;
+        }else{
+            head++;
+        }
+        
+    }
+    return maxArea;
+};
