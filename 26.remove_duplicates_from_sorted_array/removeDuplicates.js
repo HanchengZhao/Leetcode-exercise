26f1d094e3a3092d353349ba071c7e7db1705c33
Henry_Z <chancidy@gmail.com>
2016-09-11 17:01:52 +0000

    recursion solution

---
+++ 26.remove_duplicates_from_sorted_array/removeDuplicates.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    var dummy = nums[0];
+    var j = 1;
+    var i;
+    function recursion(j){//use recursion instead of forloop to avoid index change after removing the num[i]
+        for( i = j; i < nums.length; i++){
+            if(nums[i] === dummy){
+                nums.splice(i, 1);
+                 j = i
+                recursion(j);
+            }else{
+                dummy = nums[i];
+            };
+        }
+    };
+    recursion(j);
+    console.log(nums);
+    return nums.length;
+};
+//needs to get a new 'nums' as well as returning nums.length
