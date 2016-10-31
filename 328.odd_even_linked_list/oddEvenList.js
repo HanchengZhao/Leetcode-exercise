326e09053ebe401e2013c96427a2ef60c73295ea
Henry_Z <chancidy@gmail.com>
2016-10-31 20:23:47 +0000

    odd even list

---
+++ 328.odd_even_linked_list/oddEvenList.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if(head !== null){
+        var odd = head;
+        var even = head.next;
+        var evenHead = even;//the start of even
+        
+        while(even !== null && even.next !==null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+    }
+    return head;
+};
