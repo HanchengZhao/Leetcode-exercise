ad803883bd13285be3144e560ad91416269d8313
Henry_Z <chancidy@gmail.com>
2017-01-13 21:57:35 +0000

    :star: mapreduce solution

---
+++ 17.letter_combinations_of_a_phone_number/letterCombinations.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def cartesion(self, x, y):
+        combined = []
+        for i in x:
+            for j in y:
+                combined.append(i+j)
+        return combined
+
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        dict=[[''],['_'],['a','b','c'],['d','e','f'],['g','h','i'],['j','k','l'],['m','n','o'],['p','q','r','s'],['t','u','v'],['w','x','y','z']]
+        result = []
+        if(digits):
+            result = reduce(self.cartesion, map(lambda x:dict[int(x)], digits))
+        return result
+s = Solution()
+print s.letterCombinations("2")
