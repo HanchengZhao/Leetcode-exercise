811989e4507e114ad80650c00aeeaed6bca5c368
Henry_Z <chancidy@gmail.com>
2017-01-19 21:38:59 +0000

    :star::tada: two pointers

---
+++ 209.minimum_size_subarray_sum/minSubArrayLen.py
@@ -0,0 +1,24 @@
+import sys
+
+class Solution(object):
+    def minSubArrayLen(self, s, nums):
+        """
+        :type s: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        j = 0
+        Sum = 0
+        res = sys.maxint
+        for i in range(len(nums)):
+            while j < len(nums) and Sum < s:
+                Sum += nums[j]
+                j += 1
+            if Sum >= s: #the while loop can be ended when j > len(nums)
+                res = min(res, j - i) #record minimum length
+            Sum -= nums[i]
+        if res == sys.maxint:#no match
+            return 0
+        return res
+s = Solution()
+print s.minSubArrayLen(7, [2,3,1,2,4,3])
