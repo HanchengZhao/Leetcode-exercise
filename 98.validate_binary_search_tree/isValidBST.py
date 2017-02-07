2c1f3b3d974eb85f374f5af3a5952014741a7d8b
Henry_Z <chancidy@gmail.com>
2017-02-07 04:43:02 +0000

    inorder method

---
+++ 98.validate_binary_search_tree/isValidBST.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        output = []
+        self.inOrder(root, output)
+
+        for i in range(1, len(output)):
+            if output[i] <= output[i-1]:
+                return False
+        return True
+
+    def inOrder(self, parent, output):
+        if not parent:
+            return
+        self.inOrder(parent.left, output)
+        output.append(parent.val)
+        self.inOrder(parent.right, output)
