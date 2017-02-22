21123fb854c6c26bb7f06216ec40c5f127672f7d
Henry_Z <chancidy@gmail.com>
2017-02-22 03:28:25 +0000

    log(n)

---
+++ 270.closest_binary_search_tree_value/closestValue.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def closestValue(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: float
+        :rtype: int
+        """
