5945cb51253cb728b52739d2cc04d8063845eb6a
Henry_Z <chancidy@gmail.com>
2017-02-23 20:50:46 +0000

    3 pointers

---
+++ 75.sort_colors/sortColors.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        if n <= 1:
+            return
+        left, i, right = 0, 0, n-1
+        while i <= right:
+            if nums[i] == 0:
+                nums[i] = nums[left]
+                nums[left] = 0
+                left += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i] = nums[right]
+                nums[right] = 2
+                right -= 1
+            else:
+                i += 1
+        print nums
+
+if __name__ == '__main__':
+    s = Solution()
+    s.sortColors([1,2,0,1,2,0])
+
