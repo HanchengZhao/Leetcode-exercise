71530084e798c9ec907017fa6031cc29b62bc56e
Henry_Z <chancidy@gmail.com>
2017-03-09 04:20:41 +0000

    one pass, O(1) space

---
+++ 86.partition_list/partionList.py
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+
+};
