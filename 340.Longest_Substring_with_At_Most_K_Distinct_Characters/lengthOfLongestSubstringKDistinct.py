385858940acaae511560a792f9f7a680771ee4a6
Henry_Z <chancidy@gmail.com>
2017-04-17 17:49:49 +0000

    hashtable

---
+++ 340.Longest_Substring_with_At_Most_K_Distinct_Characters/lengthOfLongestSubstringKDistinct.py
@@ -0,0 +1,24 @@
+'''
+Given a string, find the length of the longest substring T that contains at most k distinct characters.
+
+For example, Given s = “eceba” and k = 2,
+
+T is "ece" which its length is 3.
+'''
+class Solution(object):
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        res, low = 0, 0
+        dic = {}
+        for i in xrange(len(s)):
+            dic[s[i]] = i
+            if len(dic) > k:
+                low = min(dic.values())
+                del dic[s[low]]
+                low += 1
+            res = max(res, i - low + 1)
+        return res
