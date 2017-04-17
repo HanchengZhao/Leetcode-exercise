cc253966d18ff5a1669cd0900a088e8a5bb874cb
Henry_Z <chancidy@gmail.com>
2017-04-17 20:22:14 +0000

    traverse from back

---
+++ 482.licence_key_formatting/licenseKeyFormatting.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def licenseKeyFormatting(self, S, K):
+        """
+        :type S: str
+        :type K: int
+        :rtype: str
+        """
+        res = ''.join(S.split("-"))
+        if not res:
+            return ""
+        count, i = 1, len(res)-1
+        while i:
+            if count < K:
+                count +=1
+            else:
+                res = res[:i] + "-" + res[i:]
+                count = 1
+            i -= 1
+        return res.upper()
+s = Solution()
+print s.licenseKeyFormatting("---",4)
