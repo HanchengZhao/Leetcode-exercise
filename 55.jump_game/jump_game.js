2dde2ce5ccdce3f8ccd93f9f0b7755f77dc29f13
Henry_Z <chancidy@gmail.com>
2017-05-01 21:08:37 +0000

    greedy

--- 55.jump_game/jump_game.js
+++ 55.jump_game/jump_game.js
@@ -4,5 +4,13 @@
  */
 var canJump = function(nums) {
     var length = nums.length;
-    var jump_len = nums[0];
+    var jump_len = 0;
+    var i = 0;
+    for (; i < length && i <= jump_len; i++) {
+        jump_len = Math.max(jump_len, nums[i] + i);
+    }
+    console.log(i);
+    return i == length;
 };
+
+console.log(canJump([0]));
