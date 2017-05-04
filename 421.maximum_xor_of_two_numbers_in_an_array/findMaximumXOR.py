66fb54c3a70af2087b11c7a4ba771c6d780bf3b2
Henry_Z <chancidy@gmail.com>
2017-05-04 21:09:52 +0000

    bit mask

---
+++ 421.maximum_xor_of_two_numbers_in_an_array/findMaximumXOR.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findMaximumXOR(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maximum, mask = 0, 0
+        # start from the most significant digit
+        # and see if there are pairs that have different values on that
+        # digit
+        for i in xrange(31, -1, -1):
+            mask = mask | (1 << i)
+            s = set()
+            for num in nums:
+                s.add(num & mask)
+            temp = maximum | (1 << i)
+            for prefix in s:
+                if temp ^ prefix in s:
+                    maximum = temp
+                    break
+        return maximum
