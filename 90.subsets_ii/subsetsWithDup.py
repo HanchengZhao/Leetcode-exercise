b7a466e541cdc1d81898e9441719424855fcd027
Henry_Z <chancidy@gmail.com>
2017-07-09 21:13:57 +0000

    iteration

---
+++ 90.subsets_ii/subsetsWithDup.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        '''
+        almost same idea as 78.subsets, just to sort the list and check whether
+        already in result everytime to remove duplicates
+        '''
+        if not nums:
+            return [[]]
+        nums.sort()
+        res = [[]]
+        index = 0
+        while index < len(nums):
+            nextarray = []
+            for i in res:
+                if (i + [nums[index]]) not in res:
+                    nextarray.append(i + [nums[index]])
+            res +=  nextarray
+            index += 1
+        return res
+
+s = Solution()
+print s.subsetsWithDup([4,4,4,1,4])
+
+# [4,4,4,1,4]
