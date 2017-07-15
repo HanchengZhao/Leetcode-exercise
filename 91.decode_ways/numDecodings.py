5ed0a3d1f77fb469d717683bedd3512ec1aa1b0a
Henry_Z <chancidy@gmail.com>
2017-07-15 22:20:55 +0000

    dp solution

---
+++ 91.decode_ways/numDecodings.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s or s[0] == "0":
+            return 0
+        length = len(s)
+        res = [0] * (length + 1) # memoization
+        res[0] = res[1] = 1 # initialize
+        for i in xrange(1, length):
+            if s[i] == "0":
+                if s[i-1] == "1" or s[i-1] == "2":
+                    res[i+1] = res[i-1]
+                else:
+                    return 0
+            elif s[i-1] != "0" and int(s[i-1: i+1]) <= 26:
+                res[i+1] = res[i-1] + res[i]
+            else:
+                res[i+1] = res[i]
+        return res[-1]
+'''
+The basic idea is dp, using an array to store all the previous combination numbers:
+res[i+1] = res[i] + res[i-1] if (previous digits could be used) else res[i].
+
+The trick part is dealing with 0, so if the digit before 0 is '1' or '2', count as
+1 combination, the digit after 0 will only be considered as 1 combination
+'''
