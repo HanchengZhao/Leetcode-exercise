ffcf38be1b5a097b9dbcf667f9c184be39acef2f
Henry_Z <chancidy@gmail.com>
2017-08-05 20:51:08 +0000

    recursion version

--- 369.plus_one_linked_list/plusOne.py
+++ 369.plus_one_linked_list/plusOne.py
@@ -6,6 +6,13 @@ Given a non-negative integer represented as non-empty a singly linked list of di
 You may assume the integer do not contain any leading zero, except the number 0 itself.
 
 The digits are stored such that the most significant digit is at the head of the list.
+
+Example:
+Input:
+1->2->3
+
+Output:
+1->2->4
 '''
 # Definition for singly-linked list.
 class ListNode(object):
