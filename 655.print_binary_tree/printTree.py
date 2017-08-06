ffd508ac03675879d3d695ecebe501a7dde2085f
Henry_Z <chancidy@gmail.com>
2017-08-06 21:28:35 +0000

    recursion

---
+++ 655.print_binary_tree/printTree.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def printTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[str]]
+        """
