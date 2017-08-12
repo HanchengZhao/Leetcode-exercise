a1e7e3be46d0cbba6db1bda314a9603fc7c37b51
Henry-Z <chancidy@gmail.com>
2017-08-12 17:32:02 -0400

    2 pointer py

---
+++ 15.3Sum/3sum.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+        for i in xrange(len(nums)-2):
+            if i == 0 or i != 0 and nums[i] != nums[i-1]: # remove duplicate first element
+                target = 0 - nums[i]
+                lo = i+1
+                hi = len(nums) - 1
+                while lo < hi:
+                    if nums[lo] + nums[hi] == target:
+                        res.append([nums[i], nums[lo], nums[hi]])
+                        while lo < hi and nums[lo + 1] == nums[lo]:  # avoid duplicate elements
+                            lo += 1
+                        while lo < hi and nums[hi - 1] == nums[hi]:
+                            hi -= 1
+                        lo += 1
+                        hi -= 1
+                    elif nums[lo] + nums[hi] < target:
+                        lo += 1
+                    else:
+                        hi -= 1
+        return res
+s = Solution()
+print s.threeSum([-1,0,1,2,-1,-4])
+            
