4569ba2e0042c2811cdd0e2deb2b184a40af5534
Henry-Z <chancidy@gmail.com>
2017-08-26 16:07:45 -0400

    facebook

---
+++ 80. Remove Duplicates from Sorted Array II/removeDuplicates.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 0
+        for n in nums:
+            if i < 2 or n > nums[i-2]: # if number is bigger than past 2 nums before i
+                nums[i] = n
+                i += 1
+        return i
+'''
+almost same idea as 19, but compare one more number
+'''
