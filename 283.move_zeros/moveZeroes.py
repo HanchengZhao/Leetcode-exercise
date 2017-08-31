b0b41d95db7d5e5bee15b63abda5049f9014776f
Henry-Z <chancidy@gmail.com>
2017-08-30 22:39:09 -0400

    reduce swap operation

--- 283.move_zeros/moveZeroes.py
+++ 283.move_zeros/moveZeroes.py
@@ -13,3 +13,20 @@ class Solution(object):
             nums[j] = 0
 # this solution takes up to O(n) in time complexity and O(1) in space, but a little bit more than n, since it
 # traverses the array and move 0s in the end
+
+# the above solution still needs n array writes, then we can reduce the write operation by only swapping when a 
+# non-zero element is found
+
+#All elements before the slow pointer (lastNonZeroFoundAt) are non-zeroes.
+#All elements between the current and slow pointer are zeroes.
+class Solution2(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        lastNonZeroIndex = 0
+        for i in xrange(len(nums)):
+            if nums[i] != 0:
+                nums[lastNonZeroIndex], nums[i] = nums[i], nums[lastNonZeroIndex]
+                lastNonZeroIndex += 1
