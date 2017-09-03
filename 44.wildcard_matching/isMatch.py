c0524888eb4db832e21cd0d514f90aff63fa6fd6
Henry-Z <chancidy@gmail.com>
2017-09-03 17:32:24 -0400

    add analysis

--- 44.wildcard_matching/isMatch.py
+++ 44.wildcard_matching/isMatch.py
@@ -16,6 +16,8 @@ class Solution(object):
                 star = p_cur
                 match = s_cur
                 p_cur += 1
+            # if we do not have a match but have a "*" before it, we record the 
+            # next char that is not '*', and find next match char in s
             elif star != -1:
                 match += 1
                 s_cur = match
@@ -30,5 +32,27 @@ class Solution(object):
             return True
         else:
             return False
+'''
+nots:
+1. remember to check  p_cur < len(p) after adding p_cur
+'''
 
+'''Analysis:
 
+For each element in s
+If *s==*p or *p == ? which means this is a match, then goes to next element s++ p++.
+If p=='*', this is also a match, but one or many chars may be available, so let us save this *'s position and the matched s position.
+If not match, then we check if there is a * previously showed up,
+       if there is no *,  return false;
+       if there is an *,  we set current p to the next element of *, and set current s to the next saved s position.
+
+e.g.
+
+abed
+?b*d**
+
+a=?, go on, b=b, go on,
+e=*, save * position star=3, save s position ss = 3, p++
+e!=d,  check if there was a *, yes, ss++, s=ss; p=star+1
+d=d, go on, meet the end.
+check the rest element in p, if all are *, true, else false;'''
