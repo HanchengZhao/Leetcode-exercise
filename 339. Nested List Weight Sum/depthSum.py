4f7ffe94836abb73416e89277337c862c69d2474
Henry-Z <chancidy@gmail.com>
2017-10-11 22:01:45 -0400

    linkedin

---
+++ 339. Nested List Weight Sum/depthSum.py
@@ -0,0 +1,54 @@
+'''
+Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Example 1:
+Given the list [[1,1],2,[1,1]], return 10. (four 1's at depth 2, one 2 at depth 1)
+
+Example 2:
+Given the list [1,[4,[6]]], return 27. (one 1 at depth 1, one 4 at depth 2, and one 6 at depth 3; 1 + 4*2 + 6*3 = 27)
+'''
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution(object):
+    def depthSum(self, nestedList):
+        """
+        :type nestedList: List[NestedInteger]
+        :rtype: int
+        """
+        return self.helper(nestedList, 1)
+    def helper(self, nestedList, depth):
+        sum = 0
+        for i in nestedList:
+            if i.isInteger():
+                sum += i.getInteger() * depth
+            else:
+                sum += self.helper(i.getList(), depth + 1)
+        return sum
+'''
+recursively go into each sublist and get the multiply of integer and depth
+'''
