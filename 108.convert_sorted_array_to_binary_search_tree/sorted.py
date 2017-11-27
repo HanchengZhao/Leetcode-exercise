81d0fb7eedda483720f3879591b9859fcafed2f1
Henry-Z <chancidy@gmail.com>
2017-11-27 00:35:17 -0500

    recursion

---
+++ 108.convert_sorted_array_to_binary_search_tree/sorted.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return None
+        mid = len(nums) / 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        return root
+        
