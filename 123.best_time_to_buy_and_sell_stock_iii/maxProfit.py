d3cca71b496b57b6e96f43c09bce740c1c98f64c
Henry Zhao <chancidy@gmail.com>
2019-07-04 16:30:37 -0400

    dp

--- 123.best_time_to_buy_and_sell_stock_iii/maxProfit.py
+++ 123.best_time_to_buy_and_sell_stock_iii/maxProfit.py
@@ -1,6 +1,20 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+# use dp to record days, transactions and stock status
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        dp = [[[0] * 2 for i in range(3)]for i in range(len(prices))]
+        # i, j, k means days, transactions, whether has stock
+        dp[0][0][0], dp[0][0][1] = 0, - prices[0]
+        dp[0][1][0], dp[0][1][1] = float("-inf"), float("-inf")
+        dp[0][2][0], dp[0][2][1] = float("-inf"), float("-inf")
+
+        for i in range(1, len(prices)):
+            dp[i][0][0] = dp[i-1][0][0]
+            dp[i][0][1] = max(dp[i-1][0][1], dp[i-1][0][0] - prices[i])
+
+            dp[i][1][0] = max(dp[i-1][0][1] + prices[i], dp[i-1][1][0])
+            dp[i][1][1] = max(dp[i-1][1][0] - prices[i], dp[i-1][1][1])
+
+            dp[i][2][0] = max(dp[i-1][1][1] + prices[i], dp[i-1][2][0])
+        return max(dp[-1][0][0], dp[-1][1][0], dp[-1][2][0])
