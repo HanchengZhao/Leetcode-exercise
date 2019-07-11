f30ea5337ad9cd9b8a9f750e98a59033fb8813f1
Henry Zhao <chancidy@gmail.com>
2019-07-11 00:13:54 -0400

    heap

--- 239.sliding_window_maximum/maxSlidingWindow_heap.py
+++ 239.sliding_window_maximum/maxSlidingWindow_heap.py
@@ -1,19 +1,22 @@
-from collections import deque
-class Solution(object):
-    def maxSlidingWindow(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        if not nums:
-            return []
-        window = deque(nums[:k])
+import heapq
+
+# Use a max-heap to keep track of the largest element.
+# Since the remove operation takes O(k), so the time complexity is O(nk)
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        window = []
         res = []
-        res.append(max(window))
-        for i in xrange(k,len(nums)):
-            window.append(nums[i])
-            window.popleft()
-            res.append(max(window))
+        for i, val in enumerate(nums):
+            if len(window) < k:
+                # save negative value for max heap
+                heapq.heappush(window, -val)
+            else:
+                window.remove(-nums[i-k])
+                heapq.heapify(window)
+                heapq.heappush(window, -val)
+            if len(window) == k:
+                # append the maximum
+                res.append(-window[0])
         return res
-# this is a brute force O(n^2) solution, there is room for optimization in max function
