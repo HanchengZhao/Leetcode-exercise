013f947bcb62ee1ece7f4bc80ffea528b183d965
Henry Zhao <chancidy@gmail.com>
2019-07-23 08:32:39 -0400

    trie

---
+++ 208.implement_trie/Trie_dict.py
@@ -0,0 +1,47 @@
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+        self.isWord = "#"
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for char in word:
+            node = node.setdefault(char, {})
+        node[self.isWord] = self.isWord
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for char in word:
+            if char in node:
+                node = node[char]
+            else:
+                return False
+        return self.isWord in node
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for char in prefix:
+            if char in node:
+                node = node[char]
+            else:
+                return False
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
