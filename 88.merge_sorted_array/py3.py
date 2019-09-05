7bca79c98496cd12141ac675e81d709148a7cc7b
Henry Zhao <chancidy@gmail.com>
2019-09-05 08:59:26 -0400

    array, 2 pointers

---
+++ 88.merge_sorted_array/py3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1, p2 = m-1, n-1
+        p = m+n-1
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] < nums2[p2]:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            else:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            p -= 1
+        # p1 might reach 0 faster than p2
+        # so we need to copy the rest in num2
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
