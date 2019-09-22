03dbbb71bf3a78840f647b4e919a0cb479a782f9
Zhao, Hancheng <hzhao@sevone.com>
2019-09-22 16:20:28 -0400

    followup

--- 332. Reconstruct Itinerary/findItinerary.py
+++ 332. Reconstruct Itinerary/findItinerary.py
@@ -24,3 +24,21 @@ class Solution:
                 self.trips[city].append(nxt)
                 self.path.pop()
         return dfs("JFK")
+
+
+'''
+Follow up question:
+What if the initial origin is not given? How would you find out the initial origin?
+
+Conditions for a directed graph:
+
+A directed graph has an eulerian circuit if and only if it is connected and each vertex has the same in-degree as out-degree. In this case we can choose any node as the start node.
+A directed graph has an eulerian trail if and only if it is connected and each vertex except 2 have the same in-degree as out-degree, and one of those 2 vertices has out-degree with one greater than in-degree (this is the start node), and the other vertex has in-degree with one greater than out-degree (this is the end node).
+
+What is the time complexity?
+
+O(Vlog(V) + E), V for nubmer of cities, E for number of tickets
+
+What is the space complexity?
+
+'''
